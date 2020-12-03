7689302988e97d9303af01507da5a4f01197d7b9
Matías Rivera <juanmatiasrivera@outlook.com>
2020-12-03 08:32:53 -0300

    server

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const connectDB = require('./config/db')
+
+
+require('dotenv').config()
+
+//routes
+const blogRoutes = require('./routes/blog')
+
+
+const app = express()
+
+//database
+connectDB()
+
+//middleware
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+//cors
+if(process.env.NODE_ENV === 'development'){
+    app.use(cors({origin: `${process.env.CLIENT_URL}`}))
+}
+//routes
+app.use('/api',blogRoutes)
+
+//routes
+app.get('/api',(req, res) => {
+    res.json({time: Date().toString()})
+})
+
+//port
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () =>{
+    console.log(`Server is running on port: ${PORT}`)
+})
