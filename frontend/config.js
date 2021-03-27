714dbab01dc2b9af20e8761b5cc79685836239a9
Matias Rivera <juanmatiasrivera@outlook.com>
2021-03-26 22:08:44 -0300

    deploy test

--- frontend/config.js
+++ frontend/config.js
@@ -2,7 +2,7 @@ import getConfig from 'next/config'
 
 const {publicRuntimeConfig} = getConfig()
 
-export const API = publicRuntimeConfig.PRODUCTION ? 'https://blog.com' : publicRuntimeConfig.API_DEV;
+export const API = publicRuntimeConfig.PRODUCTION ? publicRuntimeConfig.API_PROD : publicRuntimeConfig.API_DEV;
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
 export const DOMAIN = publicRuntimeConfig.PRODUCTION ? publicRuntimeConfig.DOMAIN_PRODUCTION : publicRuntimeConfig.DOMAIN_DEVELOPMENT; 
 export const DISQUS_SHORTNAME = publicRuntimeConfig.DISQUS_SHORTNAME;
--- frontend/next.config.example.js
+++ frontend/next.config.example.js
@@ -4,6 +4,7 @@ module.exports = withCSS({
     publicRuntimeConfig: {
         APP_NAME: 'BLOG',
         API_DEV: 'http://localhost:5000/api',
+        API_PROD: 'http://localhost:5000/api',
         PRODUCTION: false,
         DOMAIN_DEVELOPMENT: 'http://localhost:3000',
         DOMAIN_PRODUCTION: 'http://localhost:3000',
