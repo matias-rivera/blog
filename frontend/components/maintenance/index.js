bbd0d7c1f3abae50c7b4863c706ec0e3cf55c143
Matias Rivera <juanmatiasrivera@outlook.com>
2021-06-04 10:45:48 -0300

    missing style

--- frontend/components/maintenance/index.js
+++ frontend/components/maintenance/index.js
@@ -1,15 +1,19 @@
-import './maintenance.css'
+import styles from "./maintenance.module.css";
 
 const Maintenance = () => {
     return (
         <>
-            <div className='maintenance'>
-                <i className="fas fa-tools maintenace__icon"></i>
-                <h2 className='maintenance__title'>Sorry, we're down for maintenance.</h2>
-                <h6 className='maintenance__subtitle'>We'll be back shortly.</h6>
+            <div className="maintenance">
+                <i className="fas fa-tools"></i>
+                <h2 className="maintenance__title">
+                    Sorry, we're down for maintenance.
+                </h2>
+                <h6 className="maintenance__subtitle">
+                    We'll be back shortly.
+                </h6>
             </div>
         </>
-    )
-}
+    );
+};
 
-export default Maintenance
+export default Maintenance;
--- frontend/pages/blogs/index.js
+++ frontend/pages/blogs/index.js
@@ -1,165 +1,138 @@
-import { useState } from 'react'
-import Head from 'next/head'
-import Link from 'next/link'
-import { withRouter } from 'next/router';
-import Layout from '../../components/Layout'
-import { listBlogsWithCategoriesAndTags } from '../../actions/blog'
-import Card from '../../components/blog/Card'
-import { API, DOMAIN, APP_NAME  } from './../../config';
-import Categories from '../../components/categories';
-import Tags from '../../components/tags';
-import './blog.css';
+import { useState } from "react";
+import Head from "next/head";
+import Link from "next/link";
+import { withRouter } from "next/router";
+import Layout from "../../components/Layout";
+import { listBlogsWithCategoriesAndTags } from "../../actions/blog";
+import Card from "../../components/blog/Card";
+import { API, DOMAIN, APP_NAME } from "./../../config";
+import Categories from "../../components/categories";
+import Tags from "../../components/tags";
 
-const Blogs = ({blogs, categories, tags, totalBlogs, blogsLimit, blogsSkip, router}) => {
-
-
-    
-
+const Blogs = ({
+    blogs,
+    categories,
+    tags,
+    totalBlogs,
+    blogsLimit,
+    blogsSkip,
+    router,
+}) => {
     const head = () => (
         <Head>
             <title>Programming blogs | {APP_NAME}</title>
-            <meta name='description' content='Programming blogs and tutorials' />
-            <link rel='canonical' href={`${DOMAIN}${router.pathname}`}/>
-            <meta property='og:title' content={`Latest web development tutorials | ${APP_NAME}`}/>
             <meta
-                property='og:description'
-                content='addddddddddddddddddddadadadadasda'
+                name="description"
+                content="Programming blogs and tutorials"
             />
-            <meta property='og:type' content='website' />
-            <meta property='og:url' content={`${DOMAIN}${router.pathname}`} />
-            <meta property='og:site_name' content={`${APP_NAME}`} />
-            <meta property='og:image' content={`${DOMAIN}/static/images/image.jpg`}/>
-            <meta property='og:image:secure_url'  content={`${DOMAIN}/static/images/image.jpg`} />
-            <meta property='og:image:type' content='image/jpg' />
-            <meta property='fb:app_id' content={`${APP_NAME}`} />
+            <link rel="canonical" href={`${DOMAIN}${router.pathname}`} />
+            <meta
+                property="og:title"
+                content={`Latest web development tutorials | ${APP_NAME}`}
+            />
+            <meta
+                property="og:description"
+                content="addddddddddddddddddddadadadadasda"
+            />
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content={`${DOMAIN}${router.pathname}`} />
+            <meta property="og:site_name" content={`${APP_NAME}`} />
+            <meta
+                property="og:image"
+                content={`${DOMAIN}/static/images/image.jpg`}
+            />
+            <meta
+                property="og:image:secure_url"
+                content={`${DOMAIN}/static/images/image.jpg`}
+            />
+            <meta property="og:image:type" content="image/jpg" />
+            <meta property="fb:app_id" content={`${APP_NAME}`} />
         </Head>
-    )
+    );
 
-    const [limit, setLimit] = useState(blogsLimit)
-    const [skip, setSkip] = useState(0)
-    const [size, setSize] = useState(totalBlogs)
-    const [loadedBlogs, setLoadedBlogs] = useState([])
+    const [limit, setLimit] = useState(blogsLimit);
+    const [skip, setSkip] = useState(0);
+    const [size, setSize] = useState(totalBlogs);
+    const [loadedBlogs, setLoadedBlogs] = useState([]);
 
     const loadMore = () => {
-        const toSkip = skip + limit
+        const toSkip = skip + limit;
         listBlogsWithCategoriesAndTags(toSkip, limit).then((data) => {
-            if(data.error){
-                console.log(data.error)
+            if (data.error) {
+                console.log(data.error);
             } else {
-                setLoadedBlogs([...loadedBlogs, ...data.blogs])
-                setSize(data.size)
-                setSkip(toSkip)
+                setLoadedBlogs([...loadedBlogs, ...data.blogs]);
+                setSize(data.size);
+                setSkip(toSkip);
             }
-        })
-    }
+        });
+    };
 
-    const loadMoreButton = () => (
-        size > 0 && size >= limit && (
-            <button 
-                className='btn btn-outline-primary btn-lg'
+    const loadMoreButton = () =>
+        size > 0 &&
+        size >= limit && (
+            <button
+                className="btn btn-outline-primary btn-lg"
                 onClick={loadMore}
             >
                 Load More
             </button>
-        )
-    )
+        );
 
     const showAllBlogs = () => {
-        return blogs.map((blog, i) => (
-                <Card blog={blog} key={i}/>
+        return blogs.map((blog, i) => <Card blog={blog} key={i} />);
+    };
 
-        ))
-    }
-
-    const showAllCategories = () => (
-
+    const showAllCategories = () =>
         categories.map((category, i) => (
             <Link key={i} href={`/categories/${category.slug}`}>
-                <a className='btn btn-primary mx-1 mt-3'>
-                    {category.name}
-                </a>
+                <a className="btn btn-primary mx-1 mt-3">{category.name}</a>
             </Link>
-        ))
-        
-    )
+        ));
 
-    const showAllTags = () => (
-
+    const showAllTags = () =>
         tags.map((tag, i) => (
             <Link key={i} href={`/tags/${tag.slug}`}>
-                <a className='btn btn-outline-primary mx-1 mt-3'>
-                    {tag.name}
-                </a>
+                <a className="btn btn-outline-primary mx-1 mt-3">{tag.name}</a>
             </Link>
-        ))
-        
-    )
+        ));
 
-    const showLoadedBlogs = () => (
+    const showLoadedBlogs = () =>
         loadedBlogs.map((blog, i) => (
             <article key={i}>
                 <Card blog={blog} />
             </article>
-        ))
-    )
+        ));
 
-    return ( 
+    return (
         <>
             {head()}
             <Layout>
                 <Categories categories={categories} />
-                    {showAllBlogs()}
+                {showAllBlogs()}
                 <Tags tags={tags} />
-
-                {/* <main>
-                    <div className='container'>
-                        <header>
-                            <div className='col-md-12 pt-3'>
-                                <h1 className='display-4 font-weight-bold text-center'>
-                                    Programming blogs
-                                </h1>
-                                <section>
-                                    <div className='pb-5 text-center'>
-                                    {showAllCategories()}
-                                    <br/>
-                                    {showAllTags()}
-                                    </div>
-                                </section>
-                            </div>
-                        </header>
-                    </div>
-                    <div className='container'>
-                                {showAllBlogs()}
-                    </div>
-                    <div className='container'>
-                                {showLoadedBlogs()}
-                    </div>
-                <div className='text-center py-5'>
-                                {loadMoreButton()}
-                </div>
-                </main> */}
             </Layout>
         </>
-     );
-}
- 
+    );
+};
+
 Blogs.getInitialProps = () => {
-    const skip = 0
-    const limit = 20
-    return listBlogsWithCategoriesAndTags(skip, limit).then(data => {
-        if(data.error) {
-            return console.log(data.error)
+    const skip = 0;
+    const limit = 20;
+    return listBlogsWithCategoriesAndTags(skip, limit).then((data) => {
+        if (data.error) {
+            return console.log(data.error);
         } else {
             return {
-                blogs: data.blogs, 
+                blogs: data.blogs,
                 categories: data.categories,
                 tags: data.tags,
                 totalBlogs: data.size,
                 blogsLimit: limit,
-                blogsSkip: skip
-            }
+                blogsSkip: skip,
+            };
         }
-    })
-}
+    });
+};
 
 export default withRouter(Blogs);
